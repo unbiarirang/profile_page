303f5ea767200d5c587fba8bac9da6124d7c3989
최은비 <ebchoi@bulkytree.com>
2016-10-19 14:58:32 +0900

    profile page 완성

---
+++ profile.js
@@ -0,0 +1,7 @@
+var path = require('path');
+var express = require('express');
+var app = require('./app.js');
+
+app.listen(3000, function() {
+    console.log('server on!');
+});
---
+++ routes/home.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+var clicked = [0,0,0,0]
+
+/* GET users listing. */
+router.get('/', function(req, res) {
+    res.render('home', {title: 'Hey', message: 'd안녕b', my_list: my, clicked: clicked[0]});
+    ++clicked[0];
+    console.log(clicked[0]);
+});
+
+router.get('/2015', function(req, res) {
+    res.render('home_2015', {});
+});
+
+router.get('/2016', function(req, res) {
+    res.render('home_2016', {});
+});
+
+
+
+var my = ['최은비', 'eunbi choi', '崔殷庇'];
+
+module.exports = router;
